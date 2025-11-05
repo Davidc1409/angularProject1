6f5caf6775cc24c3be7998bf3b43fa2a8f945db0
David Chau <davidc2509@gmail.com>
2025-11-05 11:42:31 +0100

    Added last

--- src/app/quiz/question/question.component.ts
+++ src/app/quiz/question/question.component.ts
@@ -17,6 +17,7 @@ export class QuestionComponent implements OnInit {
   ngOnInit(): void {
     this.quizService.categoryId = this.categoryId;
     this.quizService.getQuizContentByCategory(this.categoryId);
+    console.log(this.quizContent)
   }
 
   addAnswer(answer: string, questionId: number) {
