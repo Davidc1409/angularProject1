279d9099c6d72baf53a1902c57d20476512aad02
EdenIns <e.oudaya@outlook.com>
2025-11-05 10:53:46 +0100

    add categories

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,13 +6,13 @@ import { AuthService } from "../auth/auth.service";
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  standalone: false
+  standalone: false,
 })
 export class HomeComponent implements OnInit {
   playerName = '';
   isPlayerNameConfirmed = false;
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit(): void {
     //Nous verrons plus tard comment gérer cela avec des observables
@@ -24,8 +24,8 @@ export class HomeComponent implements OnInit {
     return this.playerName.length < 1;
   }
 
-  navigateToQuiz() {
-    this.router.navigate(['/quiz', this.playerName]);
+  navigateToCategories() {
+    this.router.navigate(['/categories', this.playerName]);
   }
 
   confirmPseudo() {
